8b8dfc1b30eb11c043b369c68d70a38ba0ad4726
msalbrain <salmanmusa2020@gmail.com>
2023-05-15 18:31:16 +0100

    fixed redis connection

--- app/database/cache.py
+++ app/database/cache.py
@@ -1,10 +1,11 @@
 import redis
+from app.core import config
 
 from datetime import timedelta
 
 
 r = redis.Redis(
-    host="localhost",
-    port=6379
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT
 )
 
